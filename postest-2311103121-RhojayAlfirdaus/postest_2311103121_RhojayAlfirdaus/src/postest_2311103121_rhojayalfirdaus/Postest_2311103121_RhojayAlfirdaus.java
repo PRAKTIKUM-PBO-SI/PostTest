f2a8ba60a7d9806d430f357d904d60707afed728
rhojayalfirdaus <145036557+rhojayalfirdaus@users.noreply.github.com>
2024-10-21 09:52:08 +0700

    Add files via upload

---
+++ postest-2311103121-RhojayAlfirdaus/postest_2311103121_RhojayAlfirdaus/src/postest_2311103121_rhojayalfirdaus/Postest_2311103121_RhojayAlfirdaus.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package postest_2311103121_rhojayalfirdaus;
+
+/**
+ *
+ * @author Rhojay Alfirdaus
+ */
+public class Postest_2311103121_RhojayAlfirdaus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
